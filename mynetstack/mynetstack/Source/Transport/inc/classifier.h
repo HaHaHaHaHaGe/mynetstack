c090e0e9fd8e014a708723de0abcdebf503eaa0e
HaHaHaHaHaGe <823123988@qq.com>
2019-01-19 20:29:39 +0800

    增加数据包分类器
     /Transport/inc/classifier.h
     /Transport/src/classifier.c

---
+++ mynetstack/mynetstack/Source/Transport/inc/classifier.h
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////////////
+//创建时间：2019-1-19
+//修改时间：2018-1-19
+//创建人员：HaHaHaHaHaGe
+//修改人员：HaHaHaHaHaGe
+//主要功能：对数据包进行包分类
+///////////////////////////////////////////////////////////////////
+#ifndef __CLASSIFIER_H__
+#define __CLASSIFIER_H__
+#include "../../Factory/basic_header.h"
+#include "../../Factory/inc/basic_function.h"
+#include "../../BasicDataStream/inc/ringbuffer.h"
+#include "../../BasicDataStream/inc/packeg_taken.h"
+#endif // !__CLASSIFIER_H__
+
